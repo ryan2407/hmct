883eeee664df86b4dcae63ab020050eeb0a05d99
Ryan Murray <ryan@rmdesigns.net>
2014-12-01 20:28:40 +1000

    added login box

--- laravel/app/views/login.blade.php
+++ laravel/app/views/login.blade.php
@@ -3,21 +3,23 @@
 @section('content')
 <div class="container">
     <div class="primary">
-        <h1>Login</h1>
-        {{ Form::open(['url' => 'POST', 'url' => '/login']) }}
+        <div class="loginBox">
+            <h1>Login</h1>
+            {{ Form::open(['url' => 'POST', 'url' => '/login']) }}
 
-        <div>
-            {{ Form::label('email', 'Email') }}
-            {{ Form::text('email') }}
+            <div>
+                {{ Form::label('email', 'Email') }}
+                {{ Form::text('email') }}
+            </div>
+            <div>
+                {{ Form::label('password', 'Password') }}
+                {{ Form::password('password') }}
+            </div>
+
+            {{ Form::submit() }}
+
+            {{ Form::close() }}
         </div>
-        <div>
-            {{ Form::label('password', 'Password') }}
-            {{ Form::password('password') }}
-        </div>
-
-        {{ Form::submit() }}
-
-        {{ Form::close() }}
     </div>
     <div style="clear:both;"></div>
 </div><!-- end container -->
