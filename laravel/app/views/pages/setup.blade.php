5605ff05c4259e13b1c5114f97a60de5d6be6f83
Ryan Murray <ryan@rmdesigns.net>
2014-12-10 11:40:28 +1000

    added description to videos page

--- laravel/app/views/pages/setup.blade.php
+++ laravel/app/views/pages/setup.blade.php
@@ -14,6 +14,8 @@ identical setup procedures.
     <div class="primary">
         <h1>Hire My Camper Trailer - Camper Traile Setup Videos</h1>
         <p>See the setup videos for our camper trailers below.</p>
+        <p>Please Note each Main tent setup is slightly different depending on what camper is hired.
+            We will give you a setup demonstration on pickup.</p>
         <iframe width="663" height="403" src="//www.youtube.com/embed/bSFtsHrOSL8" frameborder="0" allowfullscreen></iframe>
     </div>
 
